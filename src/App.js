4115e451225c25f8b23c5099558a99e57c524633
ProGamer2711 <alexandurshestakov@gmail.com>
2022-07-01 18:28:13 +0300

    added random color gen and formatting conventions

--- src/App.js
+++ src/App.js
@@ -6,66 +6,83 @@ const ROWS = 4;
 const COLS = 4;
 
 function App() {
-  let [board, setBoard] = useState([]);
+    let [board, setBoard] = useState([]);
 
-  useEffect(() => {
-    const board = createInitialBoard();
-    setBoard(board);
-    setBoard(getRandomTile(board));
-    setBoard(getRandomTile(board));
-  }, []);
+    useEffect(() => {
+        const board = createInitialBoard();
+        setBoard(board);
+        setBoard(getRandomTile(board));
+        setBoard(getRandomTile(board));
+    }, []);
 
-  return (
-    <div className="board-wrapper">
-      <div className="board">
-        {board.map((row, rowIdx) => {
-          return (
-            <div className="row" key={rowIdx}>
-              {row.map(({ row, col, number }, tileIdx) => {
-                return (
-                  <Tile key={tileIdx} number={number} row={row} col={col} />
-                );
-              })}
+    return (
+        <div className="board-wrapper">
+            <div className="board">
+                {board.map((row, rowIdx) => {
+                    return (
+                        <div className="row" key={rowIdx}>
+                            {row.map(({ row, col, number }, tileIdx) => {
+                                return (
+                                    <Tile
+                                        key={tileIdx}
+                                        number={number}
+                                        row={row}
+                                        col={col}
+                                    />
+                                );
+                            })}
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 const createInitialBoard = () => {
-  const board = [];
-  for (let row = 0; row < ROWS; row++) {
-    let currentRow = [];
-    for (let col = 0; col < COLS; col++) {
-      currentRow.push(createTile(row, col));
+    const board = [];
+
+    for (let row = 0; row < ROWS; row++) {
+        let currentRow = [];
+
+        for (let col = 0; col < COLS; col++) {
+            currentRow.push(createTile(row, col));
+        }
+
+        board.push(currentRow);
     }
-    board.push(currentRow);
-  }
-  return board;
+
+    return board;
 };
 
 const createTile = (row, col) => {
-  return {
-    col,
-    row,
-    number: null,
-  };
+    return {
+        col,
+        row,
+        number: null,
+    };
 };
 
-const getRandomTile = (grid) => {
-  const emptyTiles = [];
-  grid.forEach((row) => {
-    row.forEach((tile) => {
-      if (tile.number === null) {
-        emptyTiles.push(tile);
-      }
+const getRandomTile = board => {
+    const emptyTiles = [];
+
+    board.forEach(row => {
+        row.forEach(tile => {
+            if (tile.number === null) {
+                emptyTiles.push(tile);
+            }
+        });
     });
-  });
-  let tileIdx = Math.floor(Math.random() * emptyTiles.length);
-  let selectedTile = emptyTiles[tileIdx];
-  grid[selectedTile.row][selectedTile.col].number = 2;
-  return grid;
+
+    let tileIdx = Math.floor(Math.random() * emptyTiles.length);
+    let selectedTile = emptyTiles[tileIdx];
+    let number = 2;
+
+    if (Math.floor(Math.random() * 10) === 1) number = 4;
+
+    board[selectedTile.row][selectedTile.col].number = number;
+
+    return board;
 };
+
 export default App;
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,7 +1,38 @@
 import "./Tile.css";
 
+function hashCode(str) {
+    let hash = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    return hash;
+}
+
+function hashToHex(hash) {
+    let color = (hash & 0x00ffffff).toString(16).toUpperCase();
+
+    return "00000".substring(0, 6 - color.length) + color;
+}
+
 function Tile(props) {
-  return <div className={`tile ${props.number}`}>{props?.number || " "}</div>;
+    let seed = "";
+
+    if (props?.number) {
+        for (let i = 0; i < 6; i++) {
+            seed += props?.number;
+        }
+    }
+
+    return (
+        <div
+            className="tile"
+            style={{ backgroundColor: `#${hashToHex(hashCode(seed))}` }}
+        >
+            {props?.number || " "}
+        </div>
+    );
 }
 
 export default Tile;
