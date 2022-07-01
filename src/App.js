daca6c47b6cf6da3d693e43739f897795c4be28b
ProGamer2711 <alexandurshestakov@gmail.com>
2022-07-01 14:57:21 +0300

    added Tile comp and cleaned up project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <h1>Kestakov</h1>;
+	return <h1>Borislav is gay</h1>;
 }
 
 export default App;
---
+++ src/components/Tile.js
@@ -0,0 +1,5 @@
+function Tile(props) {
+	return <div className="tile {props.number}">{props.number || "&nbsp;"}</div>;
+}
+
+export default Tile;
