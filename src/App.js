5c943b5c129da886ded8a051ba60fc0de02c877d
Borislav <borislav1207@gmail.com>
2022-07-01 15:08:23 +0300

    reder tiles

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
 import "./App.css";
+import Tile from "./components/Tile";
 import { useEffect, useState } from "react";
 
 const ROWS = 4;
 const COLS = 4;
 
 function App() {
-	let [grid, setGrid] = useState([]);
-	useEffect(() => {
-		const grid = createInitialGrid();
-		setGrid(grid);
-		console.log(grid);
-	}, []);
-	return <h1>Borislav is gay</h1>;
+  let [board, setBoard] = useState([]);
+
+  useEffect(() => {
+    const board = createInitialBoard();
+    setBoard(board);
+    console.log(board);
+  }, []);
+
+  return (
+    <div className="board">
+      {board.map((row, rowIdx) => {
+        return (
+          <div className="row" key={rowIdx}>
+            {row.map((tile, tileIdx) => {
+              return <Tile key={tileIdx} number={2} />;
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-const createInitialGrid = () => {
-	const grid = [];
-	for (let row = 0; row < ROWS; row++) {
-		let currentRow = [];
-		for (let col = 0; col < COLS; col++) {
-			currentRow.push(createTile(row, col));
-		}
-		grid.push(currentRow);
-	}
-	return grid;
+const createInitialBoard = () => {
+  const board = [];
+  for (let row = 0; row < ROWS; row++) {
+    let currentRow = [];
+    for (let col = 0; col < COLS; col++) {
+      currentRow.push(createTile(row, col));
+    }
+    board.push(currentRow);
+  }
+  return board;
 };
 
 const createTile = (row, col) => {
-	return {
-		col,
-		row,
-	};
+  return {
+    col,
+    row,
+  };
 };
 export default App;
