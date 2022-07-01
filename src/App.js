025573a17f11d4246ab11744c2e8edbc13f5fcb6
Borislav <borislav1207@gmail.com>
2022-07-01 14:43:48 +0300

    added functions for creating initial grid and adding tiles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+
+const ROWS = 4;
+const COLS = 4;
 
 function App() {
+  let [grid, setGrid] = useState([]);
+  useEffect(() => {
+    const grid = createInitialGrid();
+    setGrid(grid);
+    console.log(grid);
+  }, []);
   return <h1>Kestakov</h1>;
 }
 
+const createInitialGrid = () => {
+  const grid = [];
+  for (let row = 0; row < ROWS; row++) {
+    let currentRow = [];
+    for (let col = 0; col < COLS; col++) {
+      currentRow.push(createTile(row, col));
+    }
+    grid.push(currentRow);
+  }
+  return grid;
+};
+
+const createTile = (row, col) => {
+  return {
+    col,
+    row,
+  };
+};
 export default App;
